1b8ff9f672ff3885829d0f101ff5272e6d215b49
iovchinnikova <serira@mail.ru>
2020-09-08 18:13:03 +0300

    какие-то изменения. Тыкаю пальцами по кнопкам.

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
 const popup = document.querySelector('.popup');
 const buttonOpenPopupProfile = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__button-close')
-const popupSaveProfileButton = document.querySelector('.popup__button-save')
+const popupCloseButton = document.querySelector('.popup__button-close');
+const popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__info_title');
+const profileDescription = document.querySelector('.profile__info_subtitle');
+const popupInputName = document.querySelector('.popup__input_name');
+const popupInputDescription = document.querySelector('.popup__input_description');
 
-function openPopup(){
-  popup.classList.toggle('popup_opened');
+
+buttonOpenPopupProfile.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', onSubmitPopupForm);
+
+
+function openPopup() {
+  popupInputName.value = profileName.textContent;
+  popupInputDescription.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
 }
 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
-buttonOpenPopupProfile.addEventListener('click', openPopup)
-popupCloseButton.addEventListener('click', openPopup)
-
-function formSubmitHandler (event){
+function onSubmitPopupForm(event) {
   event.preventDefault();
 
-  console.log('formSaveHandler')
+  profileName.textContent = popupInputName.value;
+  profileDescription.textContent = popupInputDescription.value;
 
-  let profileName = document.querySelector('.profile__title');
-  let profileDescription = document.querySelector('.profile__subtitle');
-  let popupInputName = document.querySelector('.popup__input_name');
-  let popupInputDescription = document.querySelector('.popup__input_description');
-
-  popupInputName.value = profileName.textContent;
-  popupInputDescription.value = profileDescription.textContent
-
+  closePopup();
 }
-
-popupSaveProfileButton.addEventListener('submit', formSubmitHandler)
