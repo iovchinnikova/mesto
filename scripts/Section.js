cb1158242197819b1102b0a82947e4b77db39e1d
Irina Ovchinnikova <serira@mail.ru>
2020-10-25 23:07:30 +0300

    Класс Section.js

---
+++ scripts/Section.js
@@ -0,0 +1,20 @@
+export default class Section {
+  constructor({ items, renderer }, templateSelector) {
+    this._initialArray = items;
+    this._renderer = renderer;
+
+    this._container = document.querySelector(templateSelector);
+  }
+
+  //публичный метод для отрисовки всех элементов
+
+  renderItems() {
+    this._renderedItems.forEach(item => this._renderer(item));
+  }
+
+  //публичный метод, который принимает DOM-элемент и добавляет его в контейнер.
+  addItem(item) {
+    this._container.append(item);
+  }
+}
+
