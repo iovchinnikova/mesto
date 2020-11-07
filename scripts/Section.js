6646bf02b57d936859dd60b64f9fec6b677538ec
Irina Ovchinnikova <serira@mail.ru>
2020-11-07 15:32:25 +0300

    Работа 07.11

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,6 +1,6 @@
 export default class Section {
   constructor({ items, renderer }, templateSelector) {
-    this._initialArray = items;
+    this._items = initialArray;
     this._renderer = renderer;
 
     this._container = document.querySelector(templateSelector);
